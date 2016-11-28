8b0045e19f046d46e3ef91d940d83c7c81182b8b
Juan Carlos <zalapeach@gmail.com>
2016-11-28 15:16:42 -0600

    solved problem 10 of project euler

---
+++ ruby/problem_010.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+require 'pry'
+
+class SumPrimes
+  attr_accessor :limit, :prime_numbers
+
+  def initialize
+    #@limit = welcome
+    @prime_numbers = [2, 3]
+    get_prime_factors
+    puts @prime_numbers.inject(:+)
+  end
+
+  def get_prime_factors
+    #(4..@limit).each do |value|
+    (4..2000000).each do |value|
+      @prime_numbers.each do |prime|
+        break if value % prime == 0
+        @prime_numbers << value if prime == @prime_numbers.last
+      end
+    end
+  end
+
+  #def welcome
+    #puts "Get Summations of Primes"
+    #print "Limit: "
+    #STDOUT.flush
+    #gets.chomp.to_i
+  #end
+end
+
+SumPrimes.new
