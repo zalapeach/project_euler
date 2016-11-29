2fb3c276acf3338aae0f0814322153013374347a
Juan Carlos <zalapeach@gmail.com>
2016-11-28 18:04:37 -0600

    first full solution of problem 10 of project euler

--- ruby/problem_010.rb
+++ ruby/problem_010.rb
@@ -5,15 +5,14 @@ class SumPrimes
   attr_accessor :limit, :prime_numbers
 
   def initialize
-    #@limit = welcome
+    @limit = welcome
     @prime_numbers = [2, 3]
     get_prime_factors
     puts @prime_numbers.inject(:+)
   end
 
   def get_prime_factors
-    #(4..@limit).each do |value|
-    (4..2000000).each do |value|
+    (4..@limit).each do |value|
       @prime_numbers.each do |prime|
         break if value % prime == 0
         @prime_numbers << value if prime == @prime_numbers.last
@@ -21,12 +20,12 @@ class SumPrimes
     end
   end
 
-  #def welcome
-    #puts "Get Summations of Primes"
-    #print "Limit: "
-    #STDOUT.flush
-    #gets.chomp.to_i
-  #end
+  def welcome
+    puts "Get Summations of Primes"
+    print "Limit: "
+    STDOUT.flush
+    gets.chomp.to_i
+  end
 end
 
 SumPrimes.new
